4ea867d081a81b1b32685924000ac08f43f857da
unknown <UrielARMX>
2023-11-13 11:22:07 -0800

    Colaboradores

---
+++ colaboradores.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+    echo "<h2>Creador del Repositorio: Hugo Araujo</h2>";
+    echo "<h3>Colaborador en el repositorio: Uriel Armenta</h3>";
+?> 
+
+</body>
+</html>
